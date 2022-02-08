25f45994429fb24881deabd5983a0561460a7538
PamelaPz <jpamelapz02@gmail.com>
2022-02-08 10:24:59 -0600

    añade componentes y estructura

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <figure>
+          <img src="" alt="logo pz" />
+        </figure>
+      </li>
+      <li>Yo soy</li>
+      <li>Proyectos</li>
+      <li>Contáctame</li>
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/views/Rectangle.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Rectangle</div>
+</template>
+
+<style lang="stylus" scoped>
+div
+    background-color: rgba(33,170,148,.19)
+    height: 400px
+    width: 500px
+    border-radius: 20px
+</style>
